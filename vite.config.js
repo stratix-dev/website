31bf3aa90dec6be07c69cee1df9b56f28ec163d8
Andrés Carvajal <causticrez@gmail.com>
2025-11-22 00:27:09 -0300

    add vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    host: 'stratixdev.com',
-    port: 3000,
-    strictPort: true,
-    open: true
-  }
+  base:stratixdev.com
 })
